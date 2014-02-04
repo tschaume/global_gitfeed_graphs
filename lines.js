86ffa0b63fd0bad35547919b944c6485ed946228
Patrick Huck <phuck@lbl.gov>
2014-02-04 03:37:12 -0500

    lines: plotting sums, style ok

--- lines.js
+++ lines.js
@@ -1,4 +1,4 @@
-var margin = { top: 30, right: 0, bottom: 50, left: 30 };
+var margin = { top: 30, right: 0, bottom: 50, left: 120 };
 var width = 960 - margin.left - margin.right;
 var height = 430 - margin.top - margin.bottom;
 
@@ -9,11 +9,34 @@ var svg = d3.select("#chart2").append("svg")
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("data.json", function(json) {
+var x = d3.time.scale().range([0, width])
+var y = d3.scale.linear().range([height, 0])
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+var line = d3.svg.line()
+.x(function(d) { return x(d.date); })
+.y(function(d) { return y(d.lines); });
+
+d3.json("http://api.the-huck.com/gitcommits", function(json) {
   var data = [];
+  var lines = 0;
   json._items.forEach(function(item) {
     var dt = new Date(item.datetime)
-    data.push({ dt: dt, lines: item.lines })
+    if (item.lines > 2000) { console.log(item); }
+    else {
+      lines += item.lines;
+      var obj = { date: dt, lines: lines };
+      data.push(obj);
+    }
   });
-  console.log(data)
+  //console.log(data)
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.lines; }));
+  svg.append("g").attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")").call(xAxis);
+  svg.append("g").attr("class", "y axis").call(yAxis)
+  .append("text").attr("transform", "rotate(-90)")
+  .attr("y", 6).attr("dy", ".71em").style("text-anchor", "end")
+  .text("#lines changed");
+  svg.append("path").datum(data).attr("class", "line").attr("d",line);
 });
