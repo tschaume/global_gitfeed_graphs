ea45b00be9d363c48a5d03b3001e40c1eed759f4
Patrick Huck <phuck@lbl.gov>
2014-02-04 03:55:36 -0500

    protect global vars via excapsulation

--- heatmap.js
+++ heatmap.js
@@ -1,111 +1,113 @@
-var margin = { top: 30, right: 0, bottom: 50, left: 30 };
-var width = 960 - margin.left - margin.right;
-var height = 430 - margin.top - margin.bottom;
-var gridSize = Math.floor(width/24);
-var legendElWidth = gridSize*2;
-var buckets = 9;
-var colors = colorbrewer.Reds[buckets];
-var days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
-var hours = new Array(24);
-for (var j = 0; j < 24; j++) { hours[j] = j + "h"; }
+(function () {
+  var margin = { top: 30, right: 0, bottom: 50, left: 30 };
+  var width = 960 - margin.left - margin.right;
+  var height = 430 - margin.top - margin.bottom;
+  var gridSize = Math.floor(width/24);
+  var legendElWidth = gridSize*2;
+  var buckets = 9;
+  var colors = colorbrewer.Reds[buckets];
+  var days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  var hours = new Array(24);
+  for (var j = 0; j < 24; j++) { hours[j] = j + "h"; }
 
-// svg
-var svg = d3.select("#chart").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// day labels
-var dayLabels = svg.selectAll(".dayLabel")
-.data(days).enter().append("text")
-.text(function(d){return d;}).attr("x",0)
-.attr("y", function(d,i){return i*gridSize;})
-.style("text-anchor", "end")
-.attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-.attr("class", "dayLabel mono");
-// hour labels
-var hourLabels = svg.selectAll(".hourLabel")
-.data(hours).enter().append("text")
-.text(function(d){return d;}).attr("y",0)
-.attr("x", function(d,i){return i*gridSize;})
-.style("text-anchor", "middle")
-.attr("transform", "translate(" + gridSize / 2 + ", -6)")
-.attr("class", "hourLabel mono");
+  // svg
+  var svg = d3.select("#chart").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // day labels
+  var dayLabels = svg.selectAll(".dayLabel")
+  .data(days).enter().append("text")
+  .text(function(d){return d;}).attr("x",0)
+  .attr("y", function(d,i){return i*gridSize;})
+  .style("text-anchor", "end")
+  .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+  .attr("class", "dayLabel mono");
+  // hour labels
+  var hourLabels = svg.selectAll(".hourLabel")
+  .data(hours).enter().append("text")
+  .text(function(d){return d;}).attr("y",0)
+  .attr("x", function(d,i){return i*gridSize;})
+  .style("text-anchor", "middle")
+  .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+  .attr("class", "hourLabel mono");
 
-// async json api imports
-var endpoint = "http://api.the-huck.com/gitcommits"
-var url = endpoint + "?page=5"
-var last_page;
-d3.json(url, function(json) {
-  last_page = Number(json._links.last.href.split('=')[1]);
-  var q = queue(1); // run serially to preserve date order
-  for (var p = 1; p <= last_page; p++) {
-    url = endpoint
-    if (p > 1) { url += "?page=" + p; }
-    q = q.defer(d3.json, url)
-  }
-  q.awaitAll(ready)
-});
-
-// function called in awaitCall after all json api requests done
-function ready(error, jsons) {
-  // extract data of interest from json api responses
-  var raw_data = [];
-  var dates = [];
-  jsons.forEach(function(json) {
-    json._items.forEach(function(item) {
-      var dt = new Date(item.datetime)
-      dates.push(dt);
-      raw_data.push({ day: dt.getDay(), hour: dt.getHours() })
-    });
-  });
-  // sort and count commits into days/hours
-  var commits = {}
-  raw_data.forEach(function(obj) {
-    var key = JSON.stringify(obj)
-    commits[key] = (commits[key] || 0) + 1
-  });
-  console.log(commits);
-  // generate data array of objects {day,hour,value}
-  var data = [];
-  for (var i = 0; i < 7; i++) {
-    for (var j = 0; j < 24; j++) {
-      var key = JSON.stringify({ day: i, hour: j})
-      data.push({ day: i, hour: j, commits: (commits[key] || 0)})
+  // async json api imports
+  var endpoint = "http://api.the-huck.com/gitcommits"
+  var url = endpoint + "?page=5"
+  var last_page;
+  d3.json(url, function(json) {
+    last_page = Number(json._links.last.href.split('=')[1]);
+    var q = queue(1); // run serially to preserve date order
+    for (var p = 1; p <= last_page; p++) {
+      url = endpoint
+      if (p > 1) { url += "?page=" + p; }
+      q = q.defer(d3.json, url)
     }
+    q.awaitAll(ready)
+  });
+
+  // function called in awaitCall after all json api requests done
+  function ready(error, jsons) {
+    // extract data of interest from json api responses
+    var raw_data = [];
+    var dates = [];
+    jsons.forEach(function(json) {
+      json._items.forEach(function(item) {
+        var dt = new Date(item.datetime)
+        dates.push(dt);
+        raw_data.push({ day: dt.getDay(), hour: dt.getHours() })
+      });
+    });
+    // sort and count commits into days/hours
+    var commits = {}
+    raw_data.forEach(function(obj) {
+      var key = JSON.stringify(obj)
+      commits[key] = (commits[key] || 0) + 1
+    });
+    console.log(commits);
+    // generate data array of objects {day,hour,value}
+    var data = [];
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < 24; j++) {
+        var key = JSON.stringify({ day: i, hour: j})
+        data.push({ day: i, hour: j, commits: (commits[key] || 0)})
+      }
+    }
+    console.log(data)
+    // colorscale, number of commits & timeframe
+    var maxCommits = d3.max(data, function(d){return d.commits;})
+    var colorScale = d3.scale.quantile().domain([0, maxCommits]).range(colors)
+    var nrCommits = d3.sum(data, function(d){return d.commits;})
+    var timeframe = d3.extent(dates)
+    // heatmap
+    var heatMap = svg.selectAll(".hour").data(data).enter().append("rect")
+    .attr("x", function(d){return d.hour*gridSize;})
+    .attr("y", function(d){return d.day*gridSize;})
+    .attr("width", gridSize).attr("height", gridSize)
+    .attr("rx", 4).attr("ry", 4).attr("class", "hour bordered")
+    .style("fill", colors[0])
+    .transition().duration(1200)
+    .style("fill", function(d){return colorScale(d.commits);});
+    // legend
+    var legend_data = [0].concat(colorScale.quantiles())
+    var legend = svg.selectAll(".legend")
+    .data(legend_data).enter().append("g")
+    .attr("class", "legend")
+    legend.append("rect")
+    .attr("x", function(d,i){return legendElWidth*i;})
+    .attr("y", height).attr("width", legendElWidth).attr("height", gridSize/2)
+    .style("fill", function(d,i){return colors[i];});
+    legend.append("text").attr("class", "mono")
+    .text(function(d) { return "≥ " + Math.round(d); })
+    .attr("x", function(d, i) { return legendElWidth * i; })
+    .attr("y", height + gridSize);
+    legend.append("text").attr("class", "mono")
+    .text("total number of commits: " + nrCommits)
+    .attr("x", 0).attr("y", height - gridSize/2)
+    legend.append("text").attr("class", "mono")
+    .text("timeframe: " + timeframe[0] + " - " + timeframe[1])
+    .attr("x", 0).attr("y", height - gridSize)
   }
-  console.log(data)
-  // colorscale, number of commits & timeframe
-  var maxCommits = d3.max(data, function(d){return d.commits;})
-  var colorScale = d3.scale.quantile().domain([0, maxCommits]).range(colors)
-  var nrCommits = d3.sum(data, function(d){return d.commits;})
-  var timeframe = d3.extent(dates)
-  // heatmap
-  var heatMap = svg.selectAll(".hour").data(data).enter().append("rect")
-  .attr("x", function(d){return d.hour*gridSize;})
-  .attr("y", function(d){return d.day*gridSize;})
-  .attr("width", gridSize).attr("height", gridSize)
-  .attr("rx", 4).attr("ry", 4).attr("class", "hour bordered")
-  .style("fill", colors[0])
-  .transition().duration(1200)
-  .style("fill", function(d){return colorScale(d.commits);});
-  // legend
-  var legend_data = [0].concat(colorScale.quantiles())
-  var legend = svg.selectAll(".legend")
-  .data(legend_data).enter().append("g")
-  .attr("class", "legend")
-  legend.append("rect")
-  .attr("x", function(d,i){return legendElWidth*i;})
-  .attr("y", height).attr("width", legendElWidth).attr("height", gridSize/2)
-  .style("fill", function(d,i){return colors[i];});
-  legend.append("text").attr("class", "mono")
-  .text(function(d) { return "≥ " + Math.round(d); })
-  .attr("x", function(d, i) { return legendElWidth * i; })
-  .attr("y", height + gridSize);
-  legend.append("text").attr("class", "mono")
-  .text("total number of commits: " + nrCommits)
-  .attr("x", 0).attr("y", height - gridSize/2)
-  legend.append("text").attr("class", "mono")
-  .text("timeframe: " + timeframe[0] + " - " + timeframe[1])
-  .attr("x", 0).attr("y", height - gridSize)
-}
+}());
--- lines.js
+++ lines.js
@@ -1,61 +1,63 @@
-var margin = { top: 30, right: 0, bottom: 50, left: 120 };
-var width = 960 - margin.left - margin.right;
-var height = 430 - margin.top - margin.bottom;
+(function () {
+  var margin = { top: 30, right: 0, bottom: 50, left: 120 };
+  var width = 960 - margin.left - margin.right;
+  var height = 430 - margin.top - margin.bottom;
 
-// svg
-var svg = d3.select("#chart2").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // svg
+  var svg = d3.select("#chart2").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.time.scale().range([0, width])
-var y = d3.scale.linear().range([height, 0])
-var xAxis = d3.svg.axis().scale(x).orient("bottom");
-var yAxis = d3.svg.axis().scale(y).orient("left");
-var line = d3.svg.line()
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.lines); });
+  var x = d3.time.scale().range([0, width])
+  var y = d3.scale.linear().range([height, 0])
+  var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  var yAxis = d3.svg.axis().scale(y).orient("left");
+  var line = d3.svg.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.lines); });
 
-// async json api imports
-var endpoint = "http://api.the-huck.com/gitcommits"
-var url = endpoint + "?page=5"
-var last_page;
-d3.json(url, function(json) {
-  last_page = Number(json._links.last.href.split('=')[1]);
-  var q = queue();
-  for (var p = 1; p <= last_page; p++) {
-    url = endpoint
-    if (p > 1) { url += "?page=" + p; }
-    q = q.defer(d3.json, url)
-  }
-  q.awaitAll(ready)
-});
-
-// function called in awaitCall after all json api requests done
-function ready(error, jsons) {
-  var data = [];
-  var lines = 0;
-  // TODO: ordering!
-  jsons.forEach(function(json) {
-    json._items.forEach(function(item) {
-      var dt = new Date(item.datetime)
-      if (item.lines > 2000) { console.log(item); }
-      else {
-        lines += item.lines;
-        var obj = { date: dt, lines: lines };
-        data.push(obj);
-      }
-    });
+  // async json api imports
+  var endpoint = "http://api.the-huck.com/gitcommits"
+  var url = endpoint + "?page=5"
+  var last_page;
+  d3.json(url, function(json) {
+    last_page = Number(json._links.last.href.split('=')[1]);
+    var q = queue();
+    for (var p = 1; p <= last_page; p++) {
+      url = endpoint
+      if (p > 1) { url += "?page=" + p; }
+      q = q.defer(d3.json, url)
+    }
+    q.awaitAll(ready)
   });
-  //console.log(data)
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.lines; }));
-  svg.append("g").attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")").call(xAxis);
-  svg.append("g").attr("class", "y axis").call(yAxis)
-  .append("text").attr("transform", "rotate(-90)")
-  .attr("y", 6).attr("dy", ".71em").style("text-anchor", "end")
-  .text("#lines changed");
-  svg.append("path").datum(data).attr("class", "line").attr("d",line);
-}
+
+  // function called in awaitCall after all json api requests done
+  function ready(error, jsons) {
+    var data = [];
+    var lines = 0;
+    // TODO: ordering!
+    jsons.forEach(function(json) {
+      json._items.forEach(function(item) {
+        var dt = new Date(item.datetime)
+        if (item.lines > 2000) { console.log(item); }
+        else {
+          lines += item.lines;
+          var obj = { date: dt, lines: lines };
+          data.push(obj);
+        }
+      });
+    });
+    //console.log(data)
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d.lines; }));
+    svg.append("g").attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")").call(xAxis);
+    svg.append("g").attr("class", "y axis").call(yAxis)
+    .append("text").attr("transform", "rotate(-90)")
+    .attr("y", 6).attr("dy", ".71em").style("text-anchor", "end")
+    .text("#lines changed");
+    svg.append("path").datum(data).attr("class", "line").attr("d",line);
+  }
+}());
