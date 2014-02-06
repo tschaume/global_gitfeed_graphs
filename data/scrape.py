2d849a3a54c81f92d223e349328c9aec023c7880
Patrick Huck <phuck@lbl.gov>
2014-02-05 22:02:51 -0500

    also scrape projects

--- data/scrape.py
+++ data/scrape.py
@@ -2,17 +2,18 @@
 
 import requests, json
 
-DATA = None
-URL = 'http://api.the-huck.com/gitcommits'
-while True:
-  print URL
-  r = requests.get(URL)
-  print r.status_code
-  data = r.json()
-  if DATA is None: DATA = data
-  else: DATA['_items'] += data.get('_items')
-  next = data.get('_links').get('next')
-  if next is not None: URL = ''.join(['http://', next.get('href')])
-  else: break
-with open('data.txt', 'w') as outfile:
-  json.dump(DATA, outfile, indent=2)
+for endpoint in ['gitprojects', 'gitcommits']:
+  DATA = None
+  URL = 'http://api.the-huck.com/%s' % endpoint
+  while True:
+    print URL
+    r = requests.get(URL)
+    print r.status_code
+    data = r.json()
+    if DATA is None: DATA = data
+    else: DATA['_items'] += data.get('_items')
+    next = data.get('_links').get('next')
+    if next is not None: URL = ''.join(['http://', next.get('href')])
+    else: break
+  with open('data/%s.json' % endpoint, 'w') as outfile:
+    json.dump(DATA, outfile, indent=2)
